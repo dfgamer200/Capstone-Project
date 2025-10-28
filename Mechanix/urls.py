0d13614587dc4d9cbd64e9f5351c3091430c0820
dfgamer200 <dfgamer88@gmail.com>
2025-10-28 11:34:05 -0500

    further updates

--- Mechanix/urls.py
+++ Mechanix/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 
-	path('', views.welcome, name="Welcome"),
-    path('Login', views.login, name="Login"),
-    path('Troubleshoot', views.troubleshoot, name="Troubleshoot"),
-    path('FindAShop', views.findShop, name="FindAShop"),
-    path('Discussion', views.discussion, name="Discussion"),
+	path('', views.welcome, name="welcome"),
+    path('login/', views.login, name="login"),
+    path('troubleshoot/', views.troubleshoot, name="troubleshoot"),
+    path('find-shop/', views.findShop, name="find_shop"),
+    path('discussion/', views.discussion, name="discussion"),
 ]
--- Mechanix/views.py
+++ Mechanix/views.py
@@ -9,10 +9,10 @@ def login(request):
 	return render(request, "Login.html")
 
 def findShop(request):
-	return render(request, "FindAShop/.html")
+	return render(request, "FindAShop.html")
 
 def discussion(request):
-	return render(request, "Discussion/.html")
+	return render(request, "Discussion.html")
 
 def troubleshoot(request):
-	return render(request, "Troubleshoot/.html")
+	return render(request, "Troubleshoot.html")
